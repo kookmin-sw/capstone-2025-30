646df1708649c642402140b42ca0463ceff1e81b
sangkim99 <rettsadd990922@kookmin.ac.kr>
2025-03-30 01:25:58 +0900

    [server/feat] gin-initailize-apiKeyMiddleware-setting-2 : 테스트 용 AddTestStruct func update
     - 테스트 하기 위해 func 수정

--- server/internal/pkg/rest/AddTestStruct.go
+++ server/internal/pkg/rest/AddTestStruct.go
@@ -1,17 +1,22 @@
 package rest
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func AddTestStruct(c *gin.Context) {
-	// 객체 선언
+	var req struct {
+		Name  string `json:"name"`
+		Value int    `json:"value"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+		return
+	}
 
-	// panic handler
-
-	// grpcClient Open
-
-	// authClient open
-
-	// grpc func 실행
-
-	// json 반환
+	c.JSON(http.StatusOK, gin.H{
+		"message": "요청 잘 받았음!",
+		"data":    req,
+	})
 }
