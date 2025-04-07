d43f20911a2895a4e4ed2102c390d64ad746ff74
nnyouung <211_0@kookmin.ac.kr>
2025-04-07 13:20:02 +0900

    chore: 프로젝트 초기 세팅 및 공통 스타일 정의 (색상 및 폰트)

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import CustomStyles from "@/styles/CustomStyles";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div
+        style={{ ...CustomStyles.fontHead28, color: CustomStyles.primaryBlue }}
+      >
+        주문하기
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
