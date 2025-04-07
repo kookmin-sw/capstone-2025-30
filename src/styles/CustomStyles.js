ccc817ef8656d9947262b40519440094f0a6a897
nnyouung <211_0@kookmin.ac.kr>
2025-04-07 13:20:02 +0900

    chore: 프로젝트 초기 세팅 및 공통 스타일 정의 (색상 및 폰트)

---
+++ src/styles/CustomStyles.js
@@ -0,0 +1,36 @@
+const CustomStyles = {
+  primaryBlack: "#373C42",
+  primaryWhite: "#FFFFFF",
+  primaryBlue: "#458EFD",
+  pointBlur: "#B6D0FF",
+  primaryGray: "#EFEFEF",
+  pointGray: "#969696",
+  pointRed: "#EF4452",
+
+  fontHead28: {
+    fontSize: 28,
+    lineHeight: "36px",
+    fontFamily: "Pretendard",
+    fontWeight: 700,
+  },
+  fontSub16: {
+    fontSize: 16,
+    lineHeight: "24px",
+    fontFamily: "Pretendard",
+    fontWeight: 500,
+  },
+  fontBody16: {
+    fontSize: 16,
+    lineHeight: "24px",
+    fontFamily: "Pretendard",
+    fontWeight: 400,
+  },
+  fontCaption: {
+    fontSize: 12,
+    lineHeight: "16px",
+    fontFamily: "Pretendard",
+    fontWeight: 400,
+  },
+};
+
+export default CustomStyles;
