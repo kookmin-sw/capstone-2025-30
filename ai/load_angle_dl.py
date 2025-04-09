11685e25357a9c9a1c7f160f6e13f2e2079a9e85
suyeon <stgood@kookmin.ac.kr>
2025-04-09 21:01:31 +0900

    feat/sign_language_dl_focused_on_cafe-3: 수어 인식 딥러닝 모델 검증
    - load_angle_dl.py에서 실제 테스트 해볼 수 있습니다.
    - 추후 정확도 검증을 통해 적합한 threshold와 추출 작업을 진행할 예정입니다.

---
+++ ai/load_angle_dl.py
@@ -0,0 +1,114 @@
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import ImageFont, ImageDraw, Image
+
+import json
+
+with open('gesture_dict.json', 'r', encoding='utf-8') as f:
+    gesture_dict = json.load(f)
+
+actions = [gesture_dict[str(i)] for i in range(len(gesture_dict))]
+
+seq_length = 30
+
+model = tf.keras.models.load_model('90hands_angles.h5')
+print(model.input_shape)
+
+
+MAX_NUM_HANDS = 2
+mp_hands=mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands=mp_hands.Hands(
+    max_num_hands=MAX_NUM_HANDS, 
+    min_detection_confidence=0.5, 
+    min_tracking_confidence=0.5 
+)
+
+cap = cv2.VideoCapture(1)
+if not cap.isOpened():
+    print("웹캠을 열 수 없습니다.")
+    exit()
+
+
+seq = []
+action_seq = []
+
+font_path = "/Library/Fonts/AppleGothic.ttf"
+font = ImageFont.truetype(font_path, 32)
+
+
+
+while cap.isOpened():
+    ret, img = cap.read()
+    img0 = img.copy()
+
+    img = cv2.flip(img, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    if result.multi_hand_landmarks is not None:
+        for res in result.multi_hand_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
+
+            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] 
+            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] 
+            v = v2 - v1 
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            angle = np.arccos(np.einsum('nt,nt->n',
+                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) 
+
+            angle = np.degrees(angle) 
+
+            d = np.concatenate([joint.flatten(), angle])
+
+            seq.append(d)
+
+            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+            if len(seq) < seq_length:
+                continue
+
+            input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
+            print(input_data.shape) 
+
+
+            y_pred = model.predict(input_data).squeeze()
+
+            i_pred = int(np.argmax(y_pred))
+            conf = y_pred[i_pred]
+
+            if conf < 0.5:
+                continue
+
+            action = actions[i_pred]
+            action_seq.append(action)
+
+            if len(action_seq) < 3:
+                continue
+
+            this_action = '?'
+            if action_seq[-1] == action_seq[-2] == action_seq[-3]:
+                this_action = action
+
+            pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            draw = ImageDraw.Draw(pil_img)
+
+
+            text_position = (int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20))
+            draw.text(text_position, this_action, font=font, fill=(255, 255, 255))
+
+            img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
--- ai/load_dl.py
+++ ai/load_dl.py
@@ -1,38 +1,44 @@
 import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import gspread
+from google.oauth2.service_account import Credentials
+from google.auth import default
+
+import mediapipe as mp
+import base64
 import cv2
 import numpy as np
-import pandas as pd
-import mediapipe as mp
+
+
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv1D, MaxPooling1D
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import EarlyStopping
+
 from PIL import ImageFont, ImageDraw, Image
-import base64
-import time
 
-raw_data = pd.read_csv("dataset.csv")
-DATA_PATH = os.path.join('holistic_dataset')
-# gesture = {i: title for i, title in enumerate(raw_data["Title"])}
+load_dotenv()
+mongo_db_url = os.getenv("MONGO_DB_URL")
+client = MongoClient(mongo_db_url)
+db = client["dev"]
+json_key_file = os.getenv("SPREADSHEET_JSON_KEY")
 
-# 훈련을 위한 데이터 셋의 라벨링 작업
-urls = raw_data['SubDescription'][:100]
-actions = raw_data["Title"][:100].tolist()
-actions = np.array(actions)
+sign_language_collection = db["sign_language"]
 
-# 라벨 매핑
-# 테스트의 경우 : {'느끼다,느낌,뉘앙스': 0, '장애인 기능 경진 대회': 1, '똑같다,같다,동일하다': 2, '가난,곤궁,궁핍,빈곤': 3, '반복,거듭,수시로,자꾸,자주,잦다,여러 번,연거푸': 4, '가치': 5, '의사소통': 6, '걷어차다': 7, '문화재': 8, '방심,부주의': 9} 
-label_map = {label: num for num, label in enumerate(actions)}
-# print(label_map)
+name_url_dict = {
+    doc["name"]: doc["url"]
+    for doc in sign_language_collection.find({}, {"_id": 0, "name": 1, "url": 1})
+}
 
-# 영상 학습 반복 횟수, 프레임 기준 
-no_sequences = 30
-sequence_length = 30
 
-# mediapipe holistic 범위 이용 & 그리기 모듈
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
-# 한글 폰트 설정
-font_path = "/Library/Fonts/AppleGothic.ttf"
-font = ImageFont.truetype(font_path, 32)
 
 def process_frame(image_data):
     image_bytes = base64.b64decode(image_data)
@@ -51,155 +57,203 @@ def extract_keypoints(results):
         21 * 3)
     return np.concatenate([pose, lh, rh])
 
+"""
+각도 계산하는 함수
+"""
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    ba = a - b
+    bc = c - b
+
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc) + 1e-6)
+    angle = np.arccos(np.clip(cosine_angle, -1.0, 1.0))
+    return np.degrees(angle)
 
 """
-3. 예측 실행
+angle 추출하는 함수
+- 팔, 다리, 상체를 계산함
 """
-# 모델 불러오기
-best_1DCNN_model = tf.keras.models.load_model('100_mediapipe_dl.keras')
 
-# 이미지 처리 함수
-def process_frame(image_data):
-    # base64 형식의 이미지 데이터를 bytes로 디코딩
-    image_bytes = base64.b64decode(image_data)
+def extract_angles(results):
+    if not results.pose_landmarks:
+        return np.zeros(6)  # 6개 각도 기본값
 
-    # bytes를 NumPy 배열로 변환
-    np_arr = np.frombuffer(image_bytes, np.uint8)
+    landmarks = results.pose_landmarks.landmark
+    get_coord = lambda idx: [landmarks[idx].x, landmarks[idx].y, landmarks[idx].z]
 
-    # NumPy 배열을 이미지로 변환
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    angles = []
 
-    return img
+    # 팔
+    angles.append(calculate_angle(get_coord(11), get_coord(13), get_coord(15)))  # 왼팔
+    angles.append(calculate_angle(get_coord(12), get_coord(14), get_coord(16)))  # 오른팔
 
-def calculate_keypoint_change(keypoints1, keypoints2):
-    """
-    두 개의 keypoints 리스트를 비교하여 변화량을 계산하는 함수.
-    변화량은 각 keypoint 간의 유클리드 거리의 합으로 정의합니다.
-    """
-    total_change = 0
-    for kp1, kp2 in zip(keypoints1, keypoints2):
-        if kp1 is not None and kp2 is not None:
-            distance = np.linalg.norm(np.array(kp1) - np.array(kp2))
-            total_change += distance
-    return total_change
+    # 다리
+    angles.append(calculate_angle(get_coord(23), get_coord(25), get_coord(27)))  # 왼다리
+    angles.append(calculate_angle(get_coord(24), get_coord(26), get_coord(28)))  # 오른다리
 
-# 동작 인식 실행 함수
+    # 상체
+    angles.append(calculate_angle(get_coord(11), get_coord(23), get_coord(25)))  # 왼상체
+    angles.append(calculate_angle(get_coord(12), get_coord(24), get_coord(26)))  # 오른상체
+
+    return np.array(angles)
+
+font_path = "/Library/Fonts/AppleGothic.ttf"
+font = ImageFont.truetype(font_path, 32)
+
+def draw_landmarks(image, results):  # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
+
+
+def draw_styled_landmarks(image, results):
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+DATA_PATH = "keypoints"
+actions = list(name_url_dict.keys())
+best_1DCNN_model = tf.keras.models.load_model('90frames_angles.keras')
+
+# def extract_from_wc():
+#     predicted_label = ''
+#     confidence = 0.0
+#     display_counter = 0
+
+#     sequence_length = 90
+#     keypoints_list = []
+
+#     prediction_cooldown = 90  # ✅ 예측 후 90프레임 동안 재예측 X
+#     cooldown_counter = 0
+
+#     cap = cv2.VideoCapture(1)
+
+#     with mp_holistic.Holistic(min_detection_confidence=0.5,
+#                                min_tracking_confidence=0.5) as holistic:
+
+#         while True:
+#             ret, frame = cap.read()
+#             if not ret:
+#                 break
+
+#             image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#             results = holistic.process(image_rgb)
+
+#             draw_styled_landmarks(frame, results)
+
+#             keypoints = extract_keypoints(results)
+#             keypoints_list.append(keypoints)
+
+#             if cooldown_counter == 0 and len(keypoints_list) == sequence_length:
+#                 input_data = np.expand_dims(keypoints_list, axis=0)
+#                 res = best_1DCNN_model.predict(input_data)[0]
+
+#                 predicted_label = actions[np.argmax(res)]
+#                 confidence = res[np.argmax(res)]
+
+#                 print(f"✅ 예측 결과: {predicted_label} ({confidence:.2f})")
+
+#                 display_counter = 30
+#                 cooldown_counter = prediction_cooldown  # ✅ 쿨다운 시작
+#                 keypoints_list = []
+
+#             if cooldown_counter > 0:
+#                 cooldown_counter -= 1  # ✅ 쿨다운 감소
+
+#             if display_counter > 0:
+#                 pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+#                 draw = ImageDraw.Draw(pil_img)
+#                 text = f'{predicted_label} ({confidence:.2f})'
+#                 draw.text((10, 10), text, font=font, fill=(255, 0, 0))
+#                 frame = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+#                 display_counter -= 1
+
+#             cv2.imshow('Sign Language Recognition', frame)
+
+#             if cv2.waitKey(10) & 0xFF == ord('q'):
+#                 break
+
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+
 def extract_from_wc():
-    sequence = []
-    sentence = []
-    previous_keypoints = None
-    previous_result = None
-    threshold = 0.95
-    last_return_time = None 
-    to_rag_sentence_list = []  # 순서 유지용 list
-    change_threshold = 1.5  # 임계값
-    stable_count = 0
-    stable_threshold = 5  # 최소 5회 연속 같은 동작이 감지되면 확정
+    predicted_label = ''
+    confidence = 0.0
+    display_counter = 0
 
-    video = cv2.VideoCapture(1)
+    sequence_length = 90
+    angles_list = []
 
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-        while video.isOpened():
-            ret, img = video.read()
+    prediction_cooldown = 90
+    cooldown_counter = 0
 
+    cap = cv2.VideoCapture(1)
+
+    with mp_holistic.Holistic(min_detection_confidence=0.5,
+                               min_tracking_confidence=0.5) as holistic:
+
+        while True:
+            ret, frame = cap.read()
             if not ret:
                 break
-            
-            # 이미지를 base64 형식으로 인코딩
-            _, buffer = cv2.imencode('.jpg', img)
-            image_data = base64.b64encode(buffer).decode('utf-8')
 
-            # base64로 인코딩된 이미지를 process_frame 함수에 전달
-            img = process_frame(image_data)
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = holistic.process(image_rgb)
 
-            # Mediapipe 모델로 이미지 처리
-            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = holistic.process(img_rgb)
+            draw_styled_landmarks(frame, results)
 
-            # 원본 이미지 복사
-            img = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+            angles = extract_angles(results)
+            angles_list.append(angles)
 
-            # 키포인트 추출
-            keypoints = extract_keypoints(results)
-            sequence.append(keypoints)
-            sequence = sequence[-30:]  # 최대 프레임 수로 조정
+            if cooldown_counter == 0 and len(angles_list) == sequence_length:
+                input_data = np.expand_dims(angles_list, axis=0) 
+                res = best_1DCNN_model.predict(input_data)[0]
 
-            # Mediapipe 키포인트 그리기
-            if results.face_landmarks:
-                mp_drawing.draw_landmarks(img, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
-            if results.pose_landmarks:
-                mp_drawing.draw_landmarks(img, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-            if results.left_hand_landmarks:
-                mp_drawing.draw_landmarks(img, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            if results.right_hand_landmarks:
-                mp_drawing.draw_landmarks(img, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-            
-            if len(sequence) == 30:  # 동영상 길이에 맞게 예측을 처리
-
-                res = best_1DCNN_model.predict(np.expand_dims(sequence, axis=0))[0]
-                result = actions[np.argmax(res)]
+                predicted_label = actions[np.argmax(res)]
                 confidence = res[np.argmax(res)]
 
-                if (confidence > threshold) and (results.left_hand_landmarks or results.right_hand_landmarks):
-                    if len(sentence) > 0:
-                        if actions[np.argmax(res)] != sentence[-1]:
-                            sentence.append(actions[np.argmax(res)])
-                    else:
-                        sentence.append(actions[np.argmax(res)])  # 다르면 append 안한다
-                    print(result)
+                print(f"✅ 예측 결과: {predicted_label} ({confidence:.2f})")
 
-                if len(sentence) > 5:
-                    sentence = sentence[-5:]
-                
-                 # OpenCV + PIL을 이용한 한글 폰트 렌더링
-                img_pil = Image.fromarray(img)
-                draw = ImageDraw.Draw(img_pil)
+                display_counter = 30
+                cooldown_counter = prediction_cooldown
+                angles_list = []
 
-                # 이전 키포인트와의 변화량을 계산
-                if previous_keypoints is not None and (sentence and (results.left_hand_landmarks or results.right_hand_landmarks)):
-                    change = calculate_keypoint_change(previous_keypoints, keypoints)
-                    print(f"변화량: {change}")
+            if cooldown_counter > 0:
+                cooldown_counter -= 1
 
-                    # 변화량이 일정 임계값 이하일 때
-                    if change < change_threshold:  # 임계값 조정
-                        if confidence > 0.97:
-                            if result != previous_result and result not in to_rag_sentence_list[-3:]:
-                                print("!! Return 조건 충족 !!")
-                                last_return_time = time.time()                                
-                                # 2초 후에 Return 동작 표시
-                                draw.text((10, 100), f'확률: {confidence:.2f}', font=font, fill=(255, 255, 0))
-                                draw.text((10, 150), f'Return 동작: {result}', font=font, fill=(0, 255, 255))
-                                to_rag_sentence_list.append(result) 
-                                previous_result = result
+            if display_counter > 0:
+                pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+                draw = ImageDraw.Draw(pil_img)
+                text = f'{predicted_label} ({confidence:.2f})'
+                draw.text((10, 10), text, font=font, fill=(255, 0, 0))
+                frame = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+                display_counter -= 1
 
-                    draw.text((10, 100), f'확률: {confidence:.2f}', font=font, fill=(255, 255, 0))
-                    draw.text((10, 150), f'Return 동작: {result}', font=font, fill=(0, 255, 255))
-                # # 실시간으로 예측되는 수어를 확인하는 코드
-                # if last_return_time and (time.time() - last_return_time < 2):
-                #     draw.text((10, 100), f'확률: {confidence:.2f}', font=font, fill=(255, 255, 0))
-                #     draw.text((10, 150), f'Return 동작: {result}', font=font, fill=(0, 255, 255))
-                
-                previous_keypoints = keypoints
-                
-                img = np.array(img_pil)
+            cv2.imshow('Sign Language Recognition', frame)
 
-            # 화면 출력
-            cv2.imshow('Sign Language Recognition', img)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
 
-            # 키 입력에 따라 동작 설정
-            key = cv2.waitKey(1) & 0xFF
-
-            # 'q'를 눌렀을 때 종료
-            if key == ord('q'):  # 'q' 키가 눌렸을 때
-                with open("output.txt", "w", encoding="utf-8") as f:  # UTF-8로 저장
-                    for sentence in to_rag_sentence_list:
-                        f.write(sentence + "\n")  # 한 줄씩 저장
-                print("저장 완료: output.txt")
-                break  # 루프 종료
-
-
-    video.release()
+    cap.release()
     cv2.destroyAllWindows()
 
-# 실행
+
 extract_from_wc()
