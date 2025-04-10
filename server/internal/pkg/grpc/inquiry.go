bd1847c65b56f5dda97939c9d005d9857fbc69a5
sangkim99 <rettsadd990922@kookmin.ac.kr>
2025-04-10 18:39:39 +0900

    [server/feat] counter-app-grpc-stream-8 : storeId 인증(objectID 로 변환) 및 aiClient 로 데이터 변환해 전송, 응답 로직 구현

--- server/internal/pkg/grpc/inquiry.go
+++ server/internal/pkg/grpc/inquiry.go
@@ -1,12 +1,14 @@
 package grpcHandler
 
 import (
+	"encoding/json"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	pb "server/gen"
+	mauth "server/internal/pkg/database/mongodb/auth"
 	"server/internal/pkg/utils"
-	"time"
+	"sync"
 )
 
 func (s *Server) StreamInquiries(
@@ -27,10 +29,10 @@ func (s *Server) StreamInquiries(
 		}
 	}()
 
-	// 인증 (storeId -> objectID 로 변환)
-
-	var inquiryes []*pb.InquiryRequest
+	var sequence []float32
 	var totalReceived int32 = 0
+	var storeObjectID *string
+	var once sync.Once
 
 	// 데이터 스트리밍으로 받아서 축적하기
 	for {
@@ -43,20 +45,50 @@ func (s *Server) StreamInquiries(
 			panic(pb.EError_EE_INQUIRY_STREAM_FAILED)
 		}
 
-		logrus.Infof("StreamInquiries received: store_id=%s, type=%s, data=%s", req.StoreId, req.InquiryType, req.FrameData)
+		once.Do(func() {
+			storeObjectID, err = mauth.GetObjectIdInStore(req.StoreCode)
+			if err != nil {
+				logrus.Error("failed to get store object ID: ", err)
+				panic(pb.EError_EE_API_FAILED)
+			}
+		})
 
-		inquiryes = append(inquiryes, req)
+		logrus.Infof("StreamInquiries received: store_code=%s, type=%s, data=%s", req.StoreCode, req.InquiryType, req.FrameData)
+
+		var frame struct {
+			People struct {
+				PoseKeyPoints2D []float32 `json:"pose_keypoints_2d"`
+			} `json:"people"`
+		}
+
+		if err := json.Unmarshal([]byte(req.FrameData), &frame); err != nil {
+			logrus.Warn("error unmarshalling frame data: ", err)
+			continue
+		}
+
+		sequence = append(sequence, frame.People.PoseKeyPoints2D...)
 		totalReceived++
 	}
 
 	// 축적한 데이터 ai 로 전송
+	predictResp, err := s.AiClient.Predict(stream.Context(), &pb.SequenceInput{
+		Values:   sequence,
+		ClientId: *storeObjectID,
+	})
+	if err != nil {
+		logrus.Error("error calling AI client: ", err)
+		panic(stream.SendAndClose(&pb.InquiryResponse{
+			TotalReceived: totalReceived,
+			Success:       false,
+			Error:         pb.EError_EE_INQUIRY_STREAM_FAILED.Enum(),
+		}))
+	}
 
 	// 응답 받아서 클라이언트로 전송
-	time.Sleep(time.Second * 10) // 인공지능 서버에 요청하는 시간
-
 	return stream.SendAndClose(&pb.InquiryResponse{
-		TotalReceived: totalReceived,
-		Success:       true,
-		Error:         nil,
+		TotalReceived:    totalReceived,
+		Success:          true,
+		Error:            nil,
+		TranslatedKorean: predictResp.PredictedWord,
 	})
 }
