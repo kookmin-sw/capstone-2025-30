17883394688707ebc07b37e174c83b45c09a8982
sangkim99 <rettsadd990922@kookmin.ac.kr>
2025-04-21 14:31:42 +0900

    [server/feat] order-registration -5 : WebSocket Message json type 선언
    - message 에 json 으로 소통

--- server/internal/pkg/websocket/sendmessage.go
+++ server/internal/pkg/websocket/sendmessage.go
@@ -1,12 +1,14 @@
 package websocketHandler
 
 import (
+	"encoding/json"
 	"fmt"
+
 	"github.com/gorilla/websocket"
 )
 
-func SendMessageToClient(client_id, message string) error {
-	WebSocketClientsMutex.RLocker()
+func SendMessageToClient(client_id string, message WebSocketMessage) error {
+	WebSocketClientsMutex.RLock()
 	conn, ok := WebSocketClients[client_id]
 	WebSocketClientsMutex.RUnlock()
 
@@ -14,5 +16,26 @@ func SendMessageToClient(client_id, message string) error {
 		return fmt.Errorf("client not found: %s", client_id)
 	}
 
-	return conn.WriteMessage(websocket.TextMessage, []byte(message))
+	messageJSON, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %v", err)
+	}
+
+	return conn.WriteMessage(websocket.TextMessage, messageJSON)
+}
+
+type WebSocketMessage struct {
+	Type string      `json:"type"` //notification, orderMessage, inquryMessage
+	Data interface{} `json:"data"`
+}
+
+type NotificationData struct {
+	Title string `json:"title"` // order, inquiry
+	Num   int    `json:"num"`
+}
+
+type MessageData struct {
+	Num       int    `json:"num"`
+	Message   string `json:"message"`
+	CreatedAt string `json:"created_at"`
 }
