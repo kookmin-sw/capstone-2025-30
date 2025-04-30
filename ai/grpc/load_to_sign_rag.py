82f33c7d9260cf0a8d062ab0916377b987f13544
suyeon <stgood@kookmin.ac.kr>
2025-04-30 15:45:27 +0900

    ai/advencement/inqury-dl-change-frame-size-4: hotfix 잘못 올라간 json 이름

--- ai/grpc/load_to_sign_rag.py
+++ ai/grpc/load_to_sign_rag.py
@@ -36,7 +36,7 @@ for i in range(7, 16):
     grammer += (" " + data[i].page_content)
 
 # 배포용
-with open('gesture_dict/60_v1_pad_gesture_dict', 'r', encoding='utf-8') as f:
+with open('gesture_dict/60_v1_pad_gesture_dict.json', 'r', encoding='utf-8') as f:
 
 # # 로컬용
 # with open('../gesture_dict/60_v1_pad_gesture_dict.json', 'r', encoding='utf-8') as f:
