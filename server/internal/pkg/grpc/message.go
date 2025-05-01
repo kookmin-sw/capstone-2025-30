b576dfed191eee5c10bacb97c2dc6e9c488f4e12
sangkim99 <rettsadd990922@kookmin.ac.kr>
2025-05-01 15:57:45 +0900

    [server/feat] get-chatroom-list-3 : create GetChatRoomList api grpc

--- server/internal/pkg/grpc/message.go
+++ server/internal/pkg/grpc/message.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"go.mongodb.org/mongo-driver/mongo"
 	pb "server/gen"
 	mmessage "server/internal/pkg/database/mongodb/message"
 	morder "server/internal/pkg/database/mongodb/order"
 	mstore "server/internal/pkg/database/mongodb/store"
+	dbstructure "server/internal/pkg/database/structure"
 	"server/internal/pkg/utils"
+
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -36,6 +38,7 @@ func (s *Server) GetMessages(
 	}
 
 	// 주문 번호 존재 하지 않으면 panic
+	// 여기 주문 DB 에서 검색하는게 아니라 메세지 DB 에서 검색
 	_, err = morder.GetMOrder(storeId, req.Number)
 	if err != nil && errors.Is(err, mongo.ErrNoDocuments) {
 		panic(pb.EError_EE_ORDER_NOT_FOUND)
@@ -64,5 +67,67 @@ func (s *Server) GetMessages(
 		Title:    &mMessages[0].Title,
 		Number:   &req.Number,
 		Messages: pbMessages,
+	}, nil
+}
+
+func (s *Server) GetChatRoomList(
+	ctx context.Context, req *pb.GetChatRoomListRequest,
+) (response *pb.GetChatRoomListResponse, errRes error) {
+	defer func() {
+		if r := recover(); r != nil {
+			logrus.Error("defer in GetChatRoomList : ", r)
+			pbErr := utils.RecoverToEError(r, pb.EError_EE_API_FAILED)
+			response = &pb.GetChatRoomListResponse{
+				Success: false,
+				Error:   pbErr.Enum(),
+			}
+		}
+	}()
+
+	logrus.Infof("GetChatRoomList request - store_code: %s, chat_room_status: %s", req.StoreCode, req.ChatRoomStatus)
+
+	// store code -> store ID 로 변환 및 검증
+	storeId, err := mstore.ValidateStoreCodeAndGetObjectID(req.StoreCode)
+	if err != nil {
+		logrus.Errorf("Store validation failed: %v", err)
+		panic(pb.EError_EE_STORE_NOT_FOUND)
+	}
+
+	logrus.Infof("Store ID: %v", storeId)
+
+	var mNotificationMessages []dbstructure.MNotificationMessage
+	if req.ChatRoomStatus == utils.ChatRoomStatusComplete {
+		logrus.Info("Fetching completed chat rooms")
+		mNotificationMessages, err = mmessage.GetFinishedMNotificationMessageList(&storeId)
+		if err != nil {
+			logrus.Errorf("Failed to get finished messages: %v", err)
+			panic(err)
+		}
+	}
+
+	if req.ChatRoomStatus == utils.ChatRoomStatusBefore {
+		logrus.Info("Fetching ongoing chat rooms")
+		mNotificationMessages, err = mmessage.GetNotFinishedMNotificationMessageList(&storeId)
+		if err != nil {
+			logrus.Errorf("Failed to get unfinished messages: %v", err)
+			panic(err)
+		}
+	}
+
+	logrus.Infof("Found %d notification messages", len(mNotificationMessages))
+
+	pbChatRoomInfos := make([]*pb.ChatRoomInfo, len(mNotificationMessages))
+	for i, mNotificationMessage := range mNotificationMessages {
+		pbChatRoomInfos[i] = &pb.ChatRoomInfo{
+			NotificationTitle: mNotificationMessage.NotificationTitle,
+			Number:            int32(mNotificationMessage.Number),
+		}
+		logrus.Infof("Chat room %d - Title: %s, Number: %d", i, mNotificationMessage.NotificationTitle, mNotificationMessage.Number)
+	}
+
+	return &pb.GetChatRoomListResponse{
+		Success:       true,
+		Error:         nil,
+		ChatRoomInfos: pbChatRoomInfos,
 	}, nil
 }
